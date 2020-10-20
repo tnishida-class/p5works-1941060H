85a5c977fa47ac5ba3de87334ef0c2ae28105686
1941060H <65202854+1941060H@users.noreply.github.com>
2020-10-20 12:34:26 +0900

    Update sketch.js

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,17 @@
 // 小手調べ
 function setup(){
+noFill(0);
   for(let i = 0; i < 10; i++){
     // BLANK[1]
+    let s=i*10
+    ellipse(50,50,s,s);
+    if(i<5){
+      stroke(0,0,255)
+    }
+    else{
+      stroke(255,0,0)
+    }
+    //ellipse(50,50,20,20);
+    //ellipse(50,50,10,10);
+    }
   }
-}
