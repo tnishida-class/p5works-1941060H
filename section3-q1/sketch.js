69069a0228556a9aed28c141559b9bbfe7bec9ba
1941060H <65202854+1941060H@users.noreply.github.com>
2020-11-12 10:41:20 +0900

    課題

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,15 +4,18 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love GHIBLI",100,100,color(70,0,20),color(255,255,0));
 }
 
-function balloon(t){
+function balloon(t,x,y,color1,color2){//tだけじゃたりない、吹き出しが左上にしかおけなくなるから。xとかyとかをおいて位置移動できるようにする
   let w = textWidth(t);
-  let h = textAscent() + textDescent();
-  let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
-  fill(255);
-  text(t, p, h + p);
+  let h = textAscent() + textDescent();//英語の上の余白と下の余白
+  let p = 2;//余白
+  fill(color1);
+  triangle(x+w,y+(h/2),x+w-h,y+(h+p),
+      x+(w+p)+10,y+(h+p)+(h/2));
+  fill(color1);
+  rect(x, y, w + p * 4, h + p * 4);
+  fill(color2);
+  text(t, p+x, h + p+y);
 }
