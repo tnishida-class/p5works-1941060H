a93e37398a0b5e0b659e1e0781e3583fb83335de
1941060H <65202854+1941060H@users.noreply.github.com>
2020-11-23 09:53:14 +0900

    課題

--- section3-3/sketch.js
+++ section3-3/sketch.js
@@ -1,6 +1,6 @@
 // テキスト「関数を作る(2) 値を戻す関数」～「総仕上げ：カレンダーを描画しよう」
 function setup(){
-  createCanvas(200, 200);
+  createCanvas(300, 300);
   calendar(2019, 10);
 
   for(let i = 2000; i <= 2100; i++){
@@ -15,10 +15,39 @@ function setup(){
 
 function calendar(y, m){
   let dow = dayOfWeek(y, m, 1);
+  let p=20;
+  let t=0;
+  let count=0;
+  textSize(15);
+  text(y+"年"+m+"月",68,20)
+  textSize(10);
+  for(i=0;i<7,i++){
+    let a;
+    a=dayOfWeekAsString(i);
+    text(a,25*(i+1),40);
+  }
   for(let d = 1; d <= daysInMonth(y, m); d++){
+    dow = dayOfWeek(y, m, d);
+    if(dow == 6){
+      fill(0,0,255);
+    }
+    else if(dow == 0){
+      fill(255,0,0);
+    }
+    textSize(15);
+    if(d < 10){
+    text(d, (dow * 25 + p + 5), (t + 70) );
+    }
+    else text(d, (dow * 25 + p), (t + 70) );
+      fill(0);
+    if(dow == 6){
+      t += 25;
+    }
     // BLANK[3] (hint: まずは daysInYear, dayOfWeek を作ろう)
   }
 }
+
+
 
 function isLeapYear(y){
   return (y % 4 == 0) && (y % 100 != 0) || (y % 400 == 0);
@@ -26,6 +55,7 @@ function isLeapYear(y){
 
 function daysInYear(y){
   // BLANK[1]
+  return isLeapYear(y)?366:365;
 }
 
 function daysInMonth(y, m){
@@ -42,13 +72,20 @@ function daysInMonth(y, m){
 
 function dayOfYear(y, m, d){
   let count = 0;
+  for(let t = 1900; t < y; t++ ){
+    count += daysInYear(t);
+  }
   for(let i = 1; i < m; i++){
     count += daysInMonth(y, i);
   }
-  return count + d;
+  return count + d-1;
 }
 
 function dayOfWeek(y, m, d){
+  let a = dayOfYear(y, m, d) % 7;
+ console.log(a);
+ if(a==6) return 0;
+ else return a+1;
   // BLANK[2]
 }
 
--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -12,5 +12,13 @@ function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
   // BLANK[1]
+  if(keyIsDown(SHIFT)){
+    if (count < 20 || count > 40 && count < 60 || count > 80 && count < 100) size = 40;
+    else size = 60;
+  }
+  else{
+    if (count > 60) size = 40;
+    else size = 60;
+  }
   ellipse(width / 2, height / 2, size);
 }
